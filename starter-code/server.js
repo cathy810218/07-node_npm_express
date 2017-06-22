037a07a5379571bfcd78f83bf952ed1605534399
cathy810218 <cathy810218@gmail.com>
2017-06-21 19:38:56 -0700

    Create a server that uses local host

--- starter-code/server.js
+++ starter-code/server.js
@@ -10,10 +10,15 @@ const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/', function(res, req) {
+  res.sendFile('./public/new.html');
+});
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -24,5 +29,6 @@ app.post('/articles', bodyParser, function(request, response) {
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log('Current PORT:', PORT);
 });
